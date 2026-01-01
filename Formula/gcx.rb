d35f44a4dea33cc0455d7bcb70b97252574fe97b
pandaee <pandaee3356@gmail.com>
2026-01-01 15:06:22 +0800

    Fix sha256 for v1.0.0

--- Formula/gcx.rb
+++ Formula/gcx.rb
@@ -5,7 +5,7 @@ class Gcx < Formula
   desc "GCloud Context Switcher - Quick switch for GCP orgs, accounts, projects"
   homepage "https://github.com/cirrus-tools/gcx"
   url "https://github.com/cirrus-tools/gcx/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "353a14495deedc731f92429c7b51aa5b2e2af0b3b602bbac64bd9f267e17a7c7"
   license "MIT"
   head "https://github.com/cirrus-tools/gcx.git", branch: "main"
 
