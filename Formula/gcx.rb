63326a1755b51401c822baee13597bb9e40f8449
pandaee <pandaee3356@gmail.com>
2026-01-01 15:03:02 +0800

    Add gcx formula

---
+++ Formula/gcx.rb
@@ -0,0 +1,34 @@
+# typed: false
+# frozen_string_literal: true
+
+class Gcx < Formula
+  desc "GCloud Context Switcher - Quick switch for GCP orgs, accounts, projects"
+  homepage "https://github.com/cirrus-tools/gcx"
+  url "https://github.com/cirrus-tools/gcx/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "PLACEHOLDER_SHA256"
+  license "MIT"
+  head "https://github.com/cirrus-tools/gcx.git", branch: "main"
+
+  depends_on "yq"
+  depends_on "gum"
+
+  def install
+    bin.install "bin/gcx.sh" => "gcx"
+    (lib/"gcx").install "lib/gcx-setup.sh"
+  end
+
+  def caveats
+    <<~EOS
+      To get started, run:
+        gcx setup
+
+      Dependencies (install separately if needed):
+        brew install --cask google-cloud-sdk
+        brew install kubectl
+    EOS
+  end
+
+  test do
+    assert_match "gcx", shell_output("#{bin}/gcx --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# Homebrew Tap for cirrus-tools
+
+This repository contains Homebrew formulae for [cirrus-tools](https://github.com/cirrus-tools) projects.
+
+## Usage
+
+```bash
+brew tap cirrus-tools/tap
+brew install gcx
+```
+
+## Available Formulae
+
+| Formula | Description |
+|---------|-------------|
+| [gcx](Formula/gcx.rb) | GCloud Context Switcher |
